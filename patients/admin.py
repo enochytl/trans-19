d4b06b4a99e804d7c4bed140c08397346579b5d2
Rex Lee <rexlee@connect.hku.hk>
2020-05-07 22:57:43 +0800

    Customize admin page

--- patients/admin.py
+++ patients/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Group
 from .models import *
 # Register your models here.
 
 admin.site.site_header = 'Trans-19: Staff Administration Site'
 
 class PatientAdmin(admin.ModelAdmin):
-    #change_form_template(admin/
     list_display = ('name', 'id_document_no', 'date_of_birth', 'date_of_confirmation',
                     'case_no')
     list_display_links = ('name', 'id_document_no', 'case_no')
@@ -16,13 +15,18 @@ class PatientAdmin(admin.ModelAdmin):
 class LocationsAdmin(admin.ModelAdmin):
     list_display = ('name', 'start_date', 'end_date', 'Coordinates', 'Address',
                     'description', 'category')
-    
+    list_editable = ('description', 'category')
     def Coordinates(self, obj):
         return "({}, {})".format(obj.x, obj.y)
     
     def Address(self, obj):
-        return "{} {} {}".format(obj.address_line_1, obj.address_line_2, obj.address_line_3)
+        line1 = '' if (obj.address_line_1 is None) else obj.address_line_1
+        line2 = '' if (obj.address_line_2 is None) else obj.address_line_2
+        line3 = '' if (obj.address_line_3 is None) else obj.address_line_3
+        return "{} {} {}".format(line1, line2, line3) 
 
 admin.site.register(Patients, PatientAdmin)
 
 admin.site.register(Locations_visited, LocationsAdmin)
+
+admin.site.unregister(Group)
